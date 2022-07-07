12ebcbac0f12dc72940f5a1f276cffae76d7ed75
wooyoung <dndud2906@naver.com>
2022-07-07 16:21:00 +0900

    리액트 훅 useState 테스트

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem({ title, amount, date }) {
+  const [ctitle, setCtitle] = useState(title);
+
+  const clickHandler = () => {
+    setCtitle('update!!');
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{ctitle}</h2>
         <div className="expense-item__price">${amount}</div>
+        <button onClick={clickHandler}>change title!!</button>
       </div>
     </Card>
   );
